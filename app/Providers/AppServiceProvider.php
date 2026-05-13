338eee7b5a2d352f55cda5a98f0dcb0fab09e41a
Fikry <apalaginamanya123@gmail.com>
2026-05-13 13:25:27 +0700

    bismillah fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\pagination\paginator;
-//use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // URL::forceScheme('https');
+        if (env('APP_ENV') === 'production') { URL::forceScheme('https'); }
         paginator::useTailwind();
     }
 }
